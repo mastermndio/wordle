833c96db929ebdeef4f689ac7b4a1a016e5f5292
Aaron Brooks <abrooks@mastermnd.io>
2022-03-12 10:55:00 -0500

    Satisfied user story #2

--- wordle.js
+++ wordle.js
@@ -1,9 +1,24 @@
+const prompt = require("prompt-sync")();
 // Import test file as string and split it into an array
 const string = require("fs").readFileSync("./wordle-words.txt", 'utf8')
 let wordList = string.split("\n")
+
+
 
 function selectAWord(arrayOfWords) {
     return arrayOfWords[Math.floor(Math.random() * arrayOfWords.length)]
 }
 
-console.log(selectAWord(wordList))
+function getInput(userGuess) {
+    while (true) {
+        let userGuess = prompt("Please Guess a Word: ")
+
+        if (userGuess.length == 5) {
+            break
+        }
+        
+        console.log("Please enter a word with 5 letters")
+    }
+}
+
+getInput()
